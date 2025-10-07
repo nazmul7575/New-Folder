1b0299e3f9257ff82960a3a9ec9eb307a1c188a7
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-10-07 21:57:07 +0600

    Add part-3 all JavaScript

--- project-19/app.js
+++ project-19/app.js
@@ -3,32 +3,95 @@ document.addEventListener("DOMContentLoaded", main);
 const converter = {
 	area: {
 		name: "Area",
+		units: {
+			squareKm: "Square Kilometer",
+			squareM: "Square Meter",
+			squareMile: "Square Mile",
+			squareYard: "Square Yard",
+			squareFoot: "Square Foot",
+		},
 	},
 	mass: {
 		name: "Mass",
+		units: {
+			tonne: "Tonne",
+			kilogram: "Kilogram",
+			gram: "Gram",
+			miligram: "Miligram",
+		},
 	},
 	length: {
 		name: "Length",
-	},
-	volume: {
-		name: "Volume",
+		units: {
+			squareM: "Square Meter",
+			kilometer: "Kilometer",
+			meter: "Meter",
+			centimeter: "Centimeter",
+			millimeter: "Millimeter",
+		},
 	},
 	time: {
 		name: "Time",
-	},
-	pressure: {
-		name: "Pressure",
+		units: {
+			second: "Second",
+			minute: "Minute",
+			hour: "Hour",
+			day: "Day",
+		},
 	},
 };
 
 function main() {
 	const categorySelect = document.getElementById("category_select");
+	const leftSelect = document.getElementById("left_select");
+	const rightSelect = document.getElementById("right_select");
 
 	const converterKeys = Object.keys(converter).sort();
 	removeAllChild(categorySelect);
 	converterKeys.forEach((item) => {
 		addOption(categorySelect, { value: item, text: converter[item].name });
 	});
+
+	categorySelect.addEventListener("change", function () {
+		const converterName = categorySelect.value;
+		const units = converter[converterName].units;
+
+		// handle left select
+		removeAllChild(leftSelect);
+		const leftOptions = Object.keys(units);
+		leftOptions.forEach((item) => {
+			addOption(leftSelect, { value: item, text: units[item] });
+		});
+
+		// handle right select
+		removeAllChild(rightSelect);
+		const rightOptions = Object.keys(units);
+		rightOptions.forEach((item) => {
+			addOption(rightSelect, { value: item, text: units[item] });
+		});
+
+		rightSelect.value = rightOptions[1];
+	});
+
+	const converterName = categorySelect.value;
+	const units = converter[converterName].units;
+
+	// handle left select
+	removeAllChild(leftSelect);
+	const leftOptions = Object.keys(units);
+	leftOptions.forEach((item) => {
+		addOption(leftSelect, { value: item, text: units[item] });
+	});
+
+	// handle right select
+	removeAllChild(rightSelect);
+	const rightOptions = Object.keys(units);
+	rightOptions.forEach((item) => {
+		addOption(rightSelect, { value: item, text: units[item] });
+	});
+
+	rightSelect.value = rightOptions[1];
+	console.log(rightOptions[1]);
 }
 
 function addOption(parent, option) {
