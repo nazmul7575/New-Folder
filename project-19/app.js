8dc9e3a89ecaca68c6df833d073ba90f31c73401
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-10-08 11:22:22 +0600

    Add part-6 and some ls style

--- project-19/app.js
+++ project-19/app.js
@@ -10,6 +10,133 @@ const converter = {
 			squareYard: "Square Yard",
 			squareFoot: "Square Foot",
 		},
+		variants: {
+			"squareKm:squareM": {
+				formula: "multiply the area value by 1000000",
+				calculation(n) {
+					return n * 1000000;
+				},
+			},
+			"squareKm:squareMile": {
+				formula: "divide the area value by 2.59",
+				calculation(n) {
+					return n / 2.59;
+				},
+			},
+			"squareKm:squareYard": {
+				formula: "multiply the area value by 1196000",
+				calculation(n) {
+					return n * 1196000;
+				},
+			},
+			"squareKm:squareFoot": {
+				formula: "multiply the area value by 10760000",
+				calculation(n) {
+					return n * 10760000;
+				},
+			},
+			"squareM:squareKm": {
+				formula: "divide the area value by 1e+6",
+				calculation(n) {
+					return n / new Number("1e+6");
+				},
+			},
+			"squareM:squareMile": {
+				formula: "divide the area value by 2.59e+6",
+				calculation(n) {
+					return n / new Number("2.59e+6");
+				},
+			},
+			"squareM:squareYard": {
+				formula: "divide the area value by 1.196",
+				calculation(n) {
+					return n * 1.196;
+				},
+			},
+			"squareM:squareFoot": {
+				formula: "divide the area value by 10.764",
+				calculation(n) {
+					return n * 10.764;
+				},
+			},
+			"squareMile:squareKm": {
+				formula: "divide the area value by 2.59",
+				calculation(n) {
+					return n * 2.59;
+				},
+			},
+			"squareMile:squareM": {
+				formula: "divide the area value by 2.59e+6",
+				calculation(n) {
+					return n * new Number("2.59e+6");
+				},
+			},
+			"squareMile:squareYard": {
+				formula:
+					"for an approximate result, multiply the area value by 3.098e+6",
+				calculation(n) {
+					return n / new Number("3.098e+6");
+				},
+			},
+			"squareMile:squareFoot": {
+				formula:
+					"for an approximate result, multiply the area value by 2.788e+7",
+				calculation(n) {
+					return n / new Number("2.788e+7");
+				},
+			},
+			"squareYard:squareKm": {
+				formula: "divide the area value by 1.196e+6",
+				calculation(n) {
+					return n / new Number("1.196e+6");
+				},
+			},
+			"squareYard:squareM": {
+				formula: "divide the area value by 1.196",
+				calculation(n) {
+					return n / 1.196;
+				},
+			},
+			"squareYard:squareMile": {
+				formula:
+					"for an approximate result, multiply the area value by 3.098e+6",
+				calculation(n) {
+					return n / new Number("3.098e+6");
+				},
+			},
+			"squareYard:squareFoot": {
+				formula: "multiple the area value by 9",
+				calculation(n) {
+					return n / 9;
+				},
+			},
+			"squareFoot:squareKm": {
+				formula:
+					"for an approximate result, multiply the area value by 1.076e+7",
+				calculation(n) {
+					return n / new Number("1.076e+7");
+				},
+			},
+			"squareFoot:squareM": {
+				formula: "divide the area value by 10.764",
+				calculation(n) {
+					return n / 10.764;
+				},
+			},
+			"squareFoot:squareMile": {
+				formula:
+					"for an approximate result, multiply the area value by 2.788e+7",
+				calculation(n) {
+					return n / new Number("2.788e+7");
+				},
+			},
+			"squareFoot:squareYard": {
+				formula: "multiple the area value by 9",
+				calculation(n) {
+					return n / 9;
+				},
+			},
+		},
 	},
 	mass: {
 		name: "Mass",
@@ -130,3 +257,5 @@ function updateCategoryChanges(categorySelect, leftSelect, rightSelect) {
 	rightSelect.value = options[1];
 	lastRightSelectedValue = rightSelect.value;
 }
+
+function calculatevalue() {}
