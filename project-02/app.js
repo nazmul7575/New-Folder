1c568d5a091ff451a20f3afc5aa99a3b99145497
Nazmul Islam <nazmulislamsami104@gmail.com>
2025-10-12 08:46:33 +0600

    Add final JS

--- project-02/app.js
+++ project-02/app.js
@@ -6,18 +6,15 @@ function main() {
 	const output = document.getElementById("output");
 
 	btn.addEventListener("click", function () {
-		const bgColor = generateHexColor();
-
-		root.style.backgroundColor = bgColor;
-		output.value = bgColor;
+		root.style.backgroundColor = hexColor();
+		output.value = hexColor();
 	});
 }
 
-function generateHexColor() {
+function hexColor() {
 	const red = Math.floor(Math.random() * 255);
 	const green = Math.floor(Math.random() * 255);
 	const blue = Math.floor(Math.random() * 255);
 
-	// this is not clear to me, ask mehedi
 	return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
